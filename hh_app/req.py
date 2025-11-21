8b1a32ce4b4a87c4ccff64f36d43f80bdad07a51
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-11-21 23:00:15 +0500

    preparing to save the results to the database

--- hh_app/req.py
+++ hh_app/req.py
@@ -14,9 +14,63 @@ params = {
 
 response = requests.get(url=url, params=params)
 response = response.json()
-
+cont = 0
+lst = []
 print(response)
 for item in response.get('items'):
-    pprint(item.get('snippet'))
-    pprint(item.get('professional_roles'))
-    print()
+    # pprint(item.get('snippet'))
+    # pprint(item.get('professional_roles'))
+    # print()
+
+    print('___AREA___')
+    print('id = ', item.get('area').get('id'))
+    print('name = ', item.get('area').get('name'))
+
+    print('___SALARY___')
+    print('from = ', item.get('salary').get('from'))
+    print('to = ', item.get('salary').get('to'))
+    print('currency = ', item.get('salary').get('currency'))
+    print('gross = ', item.get('salary').get('gross'))
+
+    print('___EMPLOYER___')
+    print('id = ', item.get('employer').get('id'))
+    print('name = ', item.get('employer').get('name'))
+    print('url = ', item.get('employer').get('alternate_url'))
+
+
+    print('___WORK FORMAT___')
+    if item.get('work_format') == []:
+        print('code_name = ', 0)
+        print('name = ', 'Не указано')
+    else:
+        print('code_name = ', item.get('work_format')[0].get('id'))
+        print('name = ', item.get('work_format')[0].get('name'))
+
+    print('___WORK SCHEDULE___')
+    print('code_name = ', item.get('work_schedule_by_days')[0].get('id'))
+    print('name = ', item.get('work_schedule_by_days')[0].get('name'))
+    print('working_hours = ', item.get('working_hours')[0].get('name'))
+
+    print('___PROFESSIONAL ROLE___')
+    print('Количество ролей = ', item.get('professional_roles').__len__())
+    print('name = ', item.get('professional_roles')[0].get('name'))
+
+    print('___SEARCH QUERY___')
+    print('Ключевое слово = ', params.get('text'))
+
+    print('___EXPERIENCE___')
+    print('code_name = ', item.get('experience').get('id'))
+    print('name = ', item.get('experience').get('name'))
+
+    print('___VACANCY___')
+    print('hh_vacancy_id = ', item.get('id'))
+    print('name = ', item.get('name'))
+    print('published_at = ', item.get('published_at'))
+    print('hh_vacancy_id = ', item.get('alternate_url'))
+
+    if item.get('professional_roles').__len__() != 1:
+        cont += 1
+        lst.append(item.get('area').get('id'))
+
+print(cont)
+print(lst)
