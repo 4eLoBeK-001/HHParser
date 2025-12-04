1295d720cd0b59c7e030a7580d0a9d45ea58d2aa
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-12-05 01:06:48 +0500

    main page interface
    SearchQuery forms

--- hh_app/views.py
+++ hh_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from hh_parser.forms import SearchQueryForm
+
 # Create your views here.
 
 
 def main_page(request):
-    return render(request, 'hh_app/content.html')
+    if request.method == 'POST':
+        form = SearchQueryForm(request.POST)
+        if form.is_valid():
+            search_query = form.cleaned_data['name']
+    
+    context = {
+        'form': SearchQueryForm()
+    }
+    return render(request, 'hh_app/content.html', context)
+
+
---
+++ hh_parser/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from hh_app.models import SearchQuery
+
+
+class SearchQueryForm(forms.ModelForm):
+    class Meta:
+        model = SearchQuery
+        fields = ('name',)
+    
+        labels = {
+            'name': 'строка'
+        }
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input input-bordered w-full text-gray-700 text-lg bg-slate-100 focus:outline-none focus:ring-2 focus:ring-emerald-400 focus:border-emerald-400', 'placeholder': 'Введите поисковое слово...'})
+        }
