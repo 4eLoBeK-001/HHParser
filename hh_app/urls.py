fdb204f7dbd39788b493cf5e99e5f0bed690d137
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-12-05 16:39:27 +0500

    template for statistics is ready

--- hh_app/urls.py
+++ hh_app/urls.py
@@ -6,5 +6,6 @@ from hh_app import views
 app_name = 'hh_app'
 
 urlpatterns = [
-    path('home/', views.main_page)
+    path('home/', views.main_page),
+    path('vacancies/<str:search_query>/', views.detailed_statistics, name='detailed_statistics')
 ]
--- hh_app/views.py
+++ hh_app/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
+from hh_app.models import SearchQuery, Vacancy
 from hh_parser.forms import SearchQueryForm
 
 # Create your views here.
 
 
 def main_page(request):
+    vacancies = None
+    search_query = None
     if request.method == 'POST':
         form = SearchQueryForm(request.POST)
         if form.is_valid():
-            search_query = form.cleaned_data['name']
-    
+            cd_search_query = form.cleaned_data['name'].lower()
+            search_query = get_object_or_404(SearchQuery, name=cd_search_query)
+            vacancies = Vacancy.objects.filter(search_query=search_query)
+
     context = {
-        'form': SearchQueryForm()
+        'form': SearchQueryForm(),
+        'vacancies': vacancies,
+        'search_query': search_query
     }
     return render(request, 'hh_app/content.html', context)
 
 
+def detailed_statistics(request, search_query):
+    return render(request, 'hh_app/detailed_statistics.html')
