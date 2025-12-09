d840a0a929fbc8c4ae58f9cd410fa659f1cd42e7
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-12-09 21:39:10 +0500

    added the main page template

--- hh_app/urls.py
+++ hh_app/urls.py
@@ -7,7 +7,7 @@ app_name = 'hh_app'
 
 urlpatterns = [
     path('home/', views.home_page),
-    path('query/', views.searchquery_page),
+    path('query/', views.searchquery_page, name='searchquery_page'),
     path('vacancies/<str:search_query>/', views.statistics, name='statistics'),
     path('vacancies/<str:search_query>/detail/', views.detail_statistics, name='detail_statistics'),
 ]
